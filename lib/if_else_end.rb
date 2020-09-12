b772618d68dce025d268d46361d1b8969578ab1a
Amanda D <amanda@depaula.xyz>
2020-09-12 19:46:44 +0000

    Automatically backed up by Learn

--- lib/if_else_end.rb
+++ lib/if_else_end.rb
@@ -1,10 +1,10 @@
-# current_time = Time.now
-# current_time = current_time.to_i
-# if current_time.even?
-#   puts "Even!"
-# else
-#   puts "Odd!"
-# end
+current_time = Time.now
+current_time = current_time.to_i
+if current_time.even?
+  puts "Even!"
+else
+  puts "Odd!"
+end
 # greeting = "friendly_greeting"
 # case greeting
 # when "unfriendly_greeting"
@@ -12,12 +12,12 @@
 # when "friendly_greeting"
 #   puts "Hi, you are so nice!"
 # end
-current_weather = "snowing"
-case current_weather
-when "sunny"
-  puts "grab some sunscreen!"
-when "raining"
-  puts "grab an umbrella"
-when "snowing"
-  puts "bundle up"
+# current_weather = "snowing"
+# case current_weather
+# when "sunny"
+#   puts "grab some sunscreen!"
+# when "raining"
+#   puts "grab an umbrella"
+# when "snowing"
+#   puts "bundle up"
 end
