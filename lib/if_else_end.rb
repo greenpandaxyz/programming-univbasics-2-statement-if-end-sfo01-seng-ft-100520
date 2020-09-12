6c2dc2071e39bf52822bf47f50a86c419aa0a90c
Amanda D <amanda@depaula.xyz>
2020-09-12 19:42:19 +0000

    Automatically backed up by Learn

--- lib/if_else_end.rb
+++ lib/if_else_end.rb
@@ -1,7 +1,14 @@
-current_time = Time.now
-current_time = current_time.to_i
-if current_time.even?
-  puts "Even!"
-else
-  puts "Odd!"
+# current_time = Time.now
+# current_time = current_time.to_i
+# if current_time.even?
+#   puts "Even!"
+# else
+#   puts "Odd!"
+# end
+greeting = "friendly_greeting"
+case greeting
+when "unfriendly_greeting"
+  puts "You are rude!"
+when "friendly_greeting"
+  puts "Hi, you are so nice!"
 end
